e036fc51fd9b6fb4908c3b4faed1f6e6f57c6116
dazeez1 <d.azeez@alustudent.com>
2023-11-29 03:30:24 -0800

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
-        print("\t- typ: {class 'bytes'})
-        print("\t- content: b'ok')
-        print("\t- utf8 content: ok)
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
